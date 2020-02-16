3f2ed9afcf110e0c3459dcb007a18450dc6512da
Kjartan Bourgeois <kjartan.bourgeois@student.howest.be>
2020-02-16 17:25:26 +0100

    My Own methods + question A

--- prb-03-oef-01-start.wpf/MainWindow.xaml.cs
+++ prb-03-oef-01-start.wpf/MainWindow.xaml.cs
@@ -24,5 +24,65 @@ namespace prb_03_oef_01_start.wpf
         {
             InitializeComponent();
         }
+    
+        private void disableAllButtons()
+        {
+            btnAntw1a.IsEnabled = false;
+            btnAntw1b.IsEnabled = false;
+            btnAntw1c.IsEnabled = false;
+            btnAntw2a.IsEnabled = false;
+            btnAntw2b.IsEnabled = false;
+            btnAntw2c.IsEnabled = false;
+            btnAntw3a.IsEnabled = false;
+            btnAntw3b.IsEnabled = false;
+            btnAntw3c.IsEnabled = false;
+
+        }
+
+        private void disableButtonQuestionA()
+        {
+            btnAntw1a.IsEnabled = false;
+            btnAntw1b.IsEnabled = false;
+            btnAntw1c.IsEnabled = false;
+        }
+
+        private void disableButtonQuestionB()
+        {
+            btnAntw2a.IsEnabled = false;
+            btnAntw2b.IsEnabled = false;
+            btnAntw2c.IsEnabled = false;
+        }
+
+        private void disableButtonQuestionC()
+        {
+            btnAntw3a.IsEnabled = false;
+            btnAntw3b.IsEnabled = false;
+            btnAntw3c.IsEnabled = false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            wrpVraag2.Visibility = Visibility.Hidden;
+            wrpVraag3.Visibility = Visibility.Hidden;
+
+        }
+
+        private void btnAntw1c_Click(object sender, RoutedEventArgs e)
+        {
+            lblFeedBack1.Content = "Xamarin is juist";
+            wrpVraag2.Visibility = Visibility.Visible;
+            lblScore.Content = +1;
+            disableButtonQuestionA();
+
+
+
+        }
+
+        private void btnFoutAntwoordVraag1_Click(object sender, RoutedEventArgs e)
+        {
+            disableAllButtons();
+        }
+
+
     }
 }
